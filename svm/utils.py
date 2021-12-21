ef14d2f57f3eefc470bd1338906745e42b997aca
Alon <alonvbk@gmail.com>
2021-12-21 21:03:22 +0200

    Update utils.py

--- svm/utils.py
+++ svm/utils.py
@@ -12,7 +12,7 @@ class SVMClass():
         self.svm = svm.SVC(probability=True)
         self.params = params
         self.accuracy = 0
-        self.loss = 100
+        self.loss = 0
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     def gen_latent(self, dataset, num_samples):
@@ -38,23 +38,27 @@ class SVMClass():
         y = labels.tolist()
         self.svm = self.svm.fit(x, y)
 
-    def test(self, test_dataset, num_samples=10):
+    def test(self, test_dataset):
         # Testing with SVM
         latent_vec = torch.empty(0).to(self.device)
         labels = torch.empty(0).to(self.device)
 
         self.model.eval()
         dataloader = DataLoader(test_dataset, batch_size=self.params['batch_size'], shuffle=False, drop_last=True)
+        loss = torch.nn.CrossEntropyLoss()
+
         for idx, batch in enumerate(dataloader):
             batch[0] = batch[0].to(self.device)
             batch[1] = batch[1].to(self.device)
-            if idx < num_samples:
-                mu, log_var = self.model.encode(batch[0])
-                latent_vec = torch.cat((latent_vec, self.model.reparameterize(mu, log_var)))
-                labels = torch.cat((labels, batch[1]))
-            else:
-                break
-        preds = torch.tensor(self.svm.predict_proba(latent_vec.detach().cpu()))
-        self.accuracy = self.svm.score(latent_vec.detach().cpu(), labels.cpu())
-        loss = torch.nn.CrossEntropyLoss()
-        self.loss = loss(preds, torch.tensor(labels, dtype=torch.int64))
+            mu, log_var = self.model.encode(batch[0])
+
+            latent_vec = self.model.reparameterize(mu, log_var).detach().tolist()
+            labels = batch[1].tolist()
+
+            preds = torch.tensor(self.svm.predict_proba(latent_vec))
+            print(f"latent type:{type(latent_vec)}, labels type:{type(labels)}")
+            self.accuracy += self.svm.score(latent_vec, labels)
+            self.loss += loss(preds, torch.tensor(labels, dtype=torch.int64))
+
+        self.accuracy = self.accuracy / len(dataloader)
+        self.loss = self.loss / len(dataloader)
