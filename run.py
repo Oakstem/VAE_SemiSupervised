cade2eeddfab62d5fb5dbae9e54088ecf45e1786
Alon <alonvbk@gmail.com>
2021-12-27 18:17:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -14,33 +14,32 @@ from svm.svm_model import run_svm_tests
 from svm.svm_model import run_svm_train
 
 
-def train_vae():
-    args = parse_args()
-    config = get_config(args)
+args = parse_args()
+config = get_config(args.filename)
 
-    path = get_model_version(config)
-    tb_logger = TensorBoardLogger(config['logging_params']['save_dir'], name=path)
-    # For reproducibility
-    torch.manual_seed(config['logging_params']['manual_seed'])
-    np.random.seed(config['logging_params']['manual_seed'])
-    cudnn.deterministic = True
-    cudnn.benchmark = False
+path = get_model_version(config)
+tb_logger = TensorBoardLogger(config['logging_params']['save_dir'], name=path)
+# For reproducibility
+torch.manual_seed(config['logging_params']['manual_seed'])
+np.random.seed(config['logging_params']['manual_seed'])
+cudnn.deterministic = True
+cudnn.benchmark = False
 
-    model = vae_models[args.model](**config['model_params'])
-    experiment = VAEXperiment(model,
-                              config['exp_params'], config['logging_params'], config['model_params'])
-    gpus = 1 if torch.cuda.is_available() else []
-    config['trainer_params']['gpus'] = gpus
+model = vae_models[args.model](**config['model_params'])
+experiment = VAEXperiment(model,
+                          config['exp_params'], config['logging_params'], config['model_params'])
+gpus = 1 if torch.cuda.is_available() else []
+config['trainer_params']['gpus'] = gpus
 
-    runner = Trainer(default_root_dir=f"{tb_logger.save_dir}",
-                     min_epochs=1,
-                     logger=tb_logger,
-                     limit_train_batches=args.limit,
-                     limit_val_batches=args.limit,
-                     num_sanity_val_steps=1,
-                     callbacks=[SaveCallback.checkpoint_callback, SaveCallback.checkpoint_callback2, SaveCallback()],
-                     **config['trainer_params'])
+runner = Trainer(default_root_dir=f"{tb_logger.save_dir}",
+                 min_epochs=1,
+                 logger=tb_logger,
+                 limit_train_batches=args.limit,
+                 limit_val_batches=args.limit,
+                 num_sanity_val_steps=1,
+                 callbacks=[SaveCallback.checkpoint_callback, SaveCallback.checkpoint_callback2, SaveCallback()],
+                 **config['trainer_params'])
 
-    print(f"======= Training {config['model_params']['name']} =======")
-    runner.fit(experiment)
-    print(f"======= Finished Training =======")
+print(f"======= Training {config['model_params']['name']} =======")
+runner.fit(experiment)
+print(f"======= Finished Training =======")
